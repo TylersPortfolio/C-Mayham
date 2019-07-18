839c0c4d51f2a9483c00010361c57f632fff8e67
Tyler <46131742+TylersPortfolio@users.noreply.github.com>
2019-07-17 19:36:31 -0500

    Add files via upload

---
+++ Rock_Paper_Scissors.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stdlib.h>
+
+int main() {
+
+  srand(time(NULL));
+  
+  int computer = rand() % 3 + 1;
+  
+  int user = 0;
+  
+	std::cout << "====================\n";
+	std::cout << "rock paper scissors!\n";
+	std::cout << "====================\n";
+
+	std::cout << "1) ✊\n";
+	std::cout << "2) ✋\n";
+	std::cout << "3) ✌️\n";
+
+	std::cout << "shoot! \n";
+	std::cin >> user;
+  
+  if (user == 1 && computer == 1){
+  	std::cout << "Tie \n";
+  }
+	else if (user == 1 || computer == 2){
+  	std::cout << "Computer Wins \n";
+  }
+  else if (user == 1 || computer == 3){
+  	std::cout << "You Win \n";
+  }
+  else if (user == 2 || computer == 1){
+  	std::cout << "You Win \n";
+  }
+  else if (user == 2 || computer == 2){
+  	std::cout << "Tie \n";
+  }
+  else if (user == 2 || computer == 3){
+  	std::cout << "Computer Wins \n";
+  }
+  else if (user == 3 || computer == 1){
+  	std::cout << "Computer Wins \n";
+  }
+  else if (user == 3 || computer == 2){
+  	std::cout << "You Win \n";
+  }
+  else if (user == 3 || computer == 3){
+  	std::cout << "Tie \n";
+  }
+  
+}
